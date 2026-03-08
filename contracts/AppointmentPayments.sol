0d79564479187639e2dfd84aa9b65ec8e012279e
Faithfayth <169154594+Faithfayth@users.noreply.github.com>
2026-03-08 12:08:49 -0700

    updated contract

--- contracts/AppointmentPayments.sol
+++ contracts/AppointmentPayments.sol
@@ -9,10 +9,42 @@ contract AppointmentPayment {
         uint256 timestamp
     );
 
+    event MedicalRecordHashStored(
+        address indexed doctor,
+        address indexed patient,
+        string recordHash,
+        uint256 timestamp
+    );
+
+    mapping(address => string[]) public patientRecords;
+
     function payForAppointment(address doctor) external payable {
         require(doctor != address(0), "Invalid doctor address");
-        require(msg.value > 0, "Payment must be greater than zero");
+        require(msg.value > 0, "Payment required");
 
         emit AppointmentPaid(msg.sender, doctor, msg.value, block.timestamp);
+    }
+
+    function recordMedicalHash(
+        address patient,
+        string memory recordHash
+    ) external {
+        require(patient != address(0), "Invalid patient");
+        require(bytes(recordHash).length > 0, "Invalid hash");
+
+        patientRecords[patient].push(recordHash);
+
+        emit MedicalRecordHashStored(
+            msg.sender,
+            patient,
+            recordHash,
+            block.timestamp
+        );
+    }
+
+    function getPatientRecords(
+        address patient
+    ) external view returns (string[] memory) {
+        return patientRecords[patient];
     }
 }
